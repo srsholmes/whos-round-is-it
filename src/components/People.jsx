c74836e35f9e06b811047797ffbb128da85c4adb
srsholmes <srsholmes@gmail.com>
2015-09-26 00:40:53 +0100

    phrases added

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -4,16 +4,23 @@ let Reflux = require('reflux');
 let People = React.createClass({
 
   componentWillMount() {
-    this.props.drink === 'beer' ? this.ladTitle = (`Who's a lad?`) : this.ladTitle = (`Who's a lass?`);
-    this.ladTitles = ['One more round?', 'Another?', 'A quick one?', 'Still a lad?', 'Shots?', 'Go on...', 'Feeling good?', 'Pint?'];   
+    this.props.drink === 'beer' ? this.title = (`Who's a lad?`) : this.title = (`Who's a lass?`);
+    this.ladTitles = ['One more round?', 'Another?', 'A quick one?', 'Still a lad?', 'Shots?', 'Go on...', 'Feeling good?', 'Pint?'];
+    this.lassTitles = ['Another glass?', 'A little top up?', 'Be naughty'];
   },
 
   componentWillUpdate(nextProps, nextState) {
+    if (nextProps.chosenPerson === null) {
+      this.props.drink === 'beer' ? this.title = (`Who's a lad?`) : this.title = (`Who's a lass?`);
+      return;
+    }
     if (nextProps.chosenPerson !== this.props.chosenPerson) {
-      if (nextProps.people.length == 0 ) {
-        this.ladTitle = 'Oh dear...'
+      if (nextProps.people.length === 0 && nextProps.chosenPerson !== null ) {
+        this.title = 'Oh dear...'
       } else {
-        this.ladTitle = this.ladTitles[Math.floor(Math.random()*this.ladTitles.length)];
+        let sex;
+        this.props.drink === 'beer' ? sex = this.ladTitles : sex = this.lassTitles;
+        this.title = sex[Math.floor(Math.random() * sex.length)];
       }
     }
   },
@@ -23,7 +30,7 @@ let People = React.createClass({
     let person = this.props.chosenPerson;
     return (
       <div className="people">
-        <h3 className={person == null ? 'title' : 'sub-title' }>{this.ladTitle}</h3>
+        <h3 className={person == null ? 'title' : 'sub-title' }>{this.title}</h3>
          <h5 className="the-lads">{people}</h5>
       </div>
     )
