12460ea95a394c1566633f8b0bacccb9f38b98a6
srsholmes <srsholmes@gmail.com>
2015-10-09 11:12:48 +0100

    share images

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,11 +1,20 @@
 let Reflux = require('reflux');
 
+import { LOCAL_STORAGE_KEY, DRINKS } from '../constants/constants';
+
+
 let Actions = Reflux.createActions({
   'addName' : {},
   'chooseLad' : {},
   'clearLads' : {},
   'mixDrinks': {},
   'chooseDrink': {}
+});
+
+
+Actions.chooseDrink.listen((i) => {
+	document.querySelector('[property="og:image"]').setAttribute('content', `http://www.whosalad.uk/share-${DRINKS[i]}.png`);
+	console.log(document.querySelector('[property="og:image"]'));
 });
 
 export default Actions;
